81d14f5ab37aad9dd1a162c10d0f4746fee1b66f
CHETAN <chetan22vishwakarma@gmail.com>
2025-05-07 09:00:01 +0530

    Delete addition.sh

--- addition.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#set -x
-echo "Enter the integer value for num1"
-read num1
-echo "Enter the integer value for num2"
-read num2
-sum=`expr $num1 + $num2`
-echo "The sum of $num1 and $num2 is $sum"
