f1fed255f7f1295bda2a3f082a423e9bad21dc88
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 17:22:08 +0530

    Update and rename compare_number.sh to 3.compare_number.sh

--- 3.compare_number.sh
+++ 3.compare_number.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#set -x
 echo "Enter the first integer value"
 read num1
 echo "Enter the second integer value"
 read num2
+
 if [ $num1 -gt $num2 ]
 then 
   echo "$num1 is greater than $num2"
