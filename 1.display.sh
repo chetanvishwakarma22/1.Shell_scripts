dc49468788521434dd03f557fc2f2403468777ed
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 17:33:47 +0530

    Update 1.display.sh

--- 1.display.sh
+++ 1.display.sh
@@ -3,3 +3,4 @@
 echo "Welcome to Devops class"
 echo "Welcome to Docker and Kubernates"
 echo -e "Welcome to devops class \nWelcome to Docker and Kubernates \nWelcome to Git and Jenkins"
+echo "server name is $hostname"
