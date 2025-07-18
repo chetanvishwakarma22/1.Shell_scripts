60a5fed0f2f37e0c1a8d79c1b49bc420502dfce6
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 17:57:11 +0530

    Update and rename count_of_char_each_line.sh to 11_char_count_of_each_line.sh

--- 11_char_count_of_each_line.sh
+++ 11_char_count_of_each_line.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#set -x
+#set -x 
+
 echo "Enter the name of file"
 read file_name
 n=0
+
 while read line
-do 
-count=`echo "$line" | wc -c`
+do
+count=`echo "$line" | wc -c` 
 n=`expr $n + 1`
-echo "The count of characters in line $n $count"
+echo "character count of line $n is $count"
 done < $file_name
