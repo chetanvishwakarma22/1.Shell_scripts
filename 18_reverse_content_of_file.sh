86e5bc482d6361bd411254a0741c2e1082d5ab22
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 18:10:09 +0530

    Update and rename reverse_line_of_file.sh to 18_reverse_content_of_file.sh

--- 18_reverse_content_of_file.sh
+++ 18_reverse_content_of_file.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+#set -x
+
 echo "Enter the file name"
 read file_name 
+
 count=$(wc -l < $file_name)
 variable=$count
+
 while [ $count -gt 0 ]
 do 
-	head -n $count "$file_name" | tail -n 1
-	count=$((count - 1))
+        head -n $count "$file_name" | tail -n 1
+        count=$((count - 1))
 done
