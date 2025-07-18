98a0cbfaa294c2091f700cb24842fcf3d9d0d10b
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 17:55:56 +0530

    Update and rename factorial_of_number.sh to 10_factorial_of_given_number.sh

--- 10_factorial_of_given_number.sh
+++ 10_factorial_of_given_number.sh
@@ -2,10 +2,13 @@
 #set -x
 echo "Enter the integer number"
 read num
+
 sum=1
+
 while [ $num -gt 0 ]
 do 
 sum=`expr $sum \* $num`
 num=`expr $num - 1`
 done
-echo "The factorial of a given number is $sum"
+
+echo "The factorial of given number is $sum"
