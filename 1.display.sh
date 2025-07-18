26b673bdf540be65e58767305bee32922b1ed0ea
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 16:21:43 +0530

    Delete 1.display.sh

--- 1.display.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#set -x
-echo "Welcome to Devops class"
-echo "Welcome to Docker and Kubernates"
-echo -e "Welcome to devops class \nWelcome to Docker and Kubernates \nWelcome to Git and Jenkins"
-echo "The command line arguments are $0,$1,$2,$3,$*,$@,$#"
