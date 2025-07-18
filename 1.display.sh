1360619132fd2cbb056b73bb211f712d9684d334
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 17:33:06 +0530

    Update 1.display.sh

--- 1.display.sh
+++ 1.display.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #set -x
+
 echo "Welcome to Devops class"
+
 echo "Welcome to Docker and Kubernates"
+
 echo -e "Welcome to devops class \nWelcome to Docker and Kubernates \nWelcome to Git and Jenkins"
